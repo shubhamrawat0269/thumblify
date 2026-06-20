51a07aaa9dcada656dd8fdca24881bd2734dd534
Shubham Rawat <rawatshubam269@gmail.com>
2026-06-20 15:48:12 +0530

    added : user model created

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,41 @@ import "dotenv/config";
 import cors from "cors";
 import express, { Request, Response } from "express";
 import connectDB from "./config/connectDB.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+
+declare module "express-session" {
+  interface sessionData {
+    isLoggedIn: boolean;
+    userId: string;
+  }
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    credentials: true,
+  }),
+);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI as string,
+      collectionName: "sessions",
+    }),
+  }),
+);
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
